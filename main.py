1c42a67ec985717bf1f4f6b4c87fa0e652ec5a6a
mehboob737426 <150647224+mehboob737426@users.noreply.github.com>
2024-01-18 11:31:28 +0500

    fix

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 print("hello world")
 
+
 def welcome():
     print("welcome to MMM")
 
+def welcome2():
+    print("welcome to MMM 2")
+
 
 print("this is a tutorial for github actions")
-  welcome();
+welcome();
